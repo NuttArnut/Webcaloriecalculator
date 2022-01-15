70f4ee6982015419811bf432f3041a617ecc5511
unknown <arnutbuadonpai@gmail.com>
2022-01-15 18:51:47 +0700

    first commit

---
+++ public/app.js
@@ -0,0 +1,10 @@
+function toMenProgram(Weight)
+{
+    let value = (Weight * 31)
+    document.getElementById("result").innerHTML = value + " แคลอรี่";
+}
+function toWomenProgram(Weight)
+{
+    let value = (Weight * 27)
+    document.getElementById("result").innerHTML = value + " แคลอรี่";
+}
